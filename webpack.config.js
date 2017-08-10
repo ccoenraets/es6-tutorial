8c4c331d5689ee80f8b033ebdd00b978eaaeba60
Maria Romanova <aemulare@users.noreply.github.com>
2017-08-09 22:24:03 -0400

    Finished JS tutorial.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+ var webpack = require('webpack');
+
+ module.exports = {
+     entry: {
+        app: './js/main.js',
+        ratefinder: './js/ratefinder.js'
+     },
+     output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].bundle.js'
+     },
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['es2015']
+                 }
+             }
+         ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
